43d4f42c62a64964cf8fa1fd1dcbbf93970c2c95
Murty Gudipati <mgudipati@saventech.com>
2014-05-22 13:19:58 +0530

    Initial Version

---
+++ plot1.R
@@ -0,0 +1,20 @@
+## Read serialized objects
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+## Convert into data.table and cleanup for memory savings
+library(data.table)
+NEI.DT = data.table(NEI)
+rm(NEI)
+
+## Aggregate Emissions by year
+total.emissions = NEI.DT[, sum(Emissions), by="year"]
+
+# Open the PNG device
+png(filename="plot1.png", width=480, height=480, units="px")
+
+## Plot emissions per year using basic package
+plot(total.emissions, type = "b", pch = 19, col = "blue", ylab = "Emissions", xlab = "Year", main = "Total Emissions per Year")
+
+# Close the PNG device
+dev.off()
